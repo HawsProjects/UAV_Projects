cd9950e2d410c90be6300fb2c6123d25e93065ce
HawsProjects <hawsengineering@gmail.com>
2023-08-02 19:09:20 -0500

    anotha

--- servo_pi.py
+++ servo_pi.py
@@ -5,6 +5,6 @@ servo = 18
  
 pwm = pigpio.pi() 
 pwm.set_mode(servo, pigpio.OUTPUT)
- 
-pwm.set_PWM_frequency(servo, 700)
+
+pwm.set_PWM_frequency(servo, 100)
 pwm.set_PWM_range(servo, 2000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
