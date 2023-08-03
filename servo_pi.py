1254f50e79cc925b4bc2d8506122ee77c7ab70a5
HawsProjects <hawsengineering@gmail.com>
2023-08-02 19:00:20 -0500

    servo change

--- servo_pi.py
+++ servo_pi.py
@@ -6,5 +6,5 @@ servo = 18
 pwm = pigpio.pi() 
 pwm.set_mode(servo, pigpio.OUTPUT)
  
-pwm.set_PWM_frequency(servo, 50)
-pwm.set_PWM_range(servo, 20000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
+pwm.set_PWM_frequency(servo, 700)
+pwm.set_PWM_range(servo, 2000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
